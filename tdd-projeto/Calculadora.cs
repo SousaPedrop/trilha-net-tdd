230029aec94b2d74dce4b3788357a5b4b9cb9ed9
SousaPedrop <sousapedro.work@gmail.com>
2023-11-02 23:59:05 -0300

    Criando classe Calculadora

---
+++ tdd-projeto/Calculadora.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tdd_projeto
+{
+    public class Calculadora
+    {
+        public int Somar(int num1, int num2)
+        {
+            return 0;
+        }
+
+        public int Subtrair(int num1, int num2)
+        {
+            return 0;
+        }
+
+        public int Multiplicar(int num1, int num2)
+        {
+            return 0;
+        }
+
+        public int Dividir(int num1, int num2)
+        {
+            return 0;
+        }
+        
+        public List<string> RetornarHistorico(int num1, int num2)
+        {
+            return new List<string>();
+        }
+    }
+}
